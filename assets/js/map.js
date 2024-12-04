eee9ae656914a135af7958d32fdedd908f1cfa28
Sergio GML <sergiogarciamanzanares@gmail.com>
2024-12-04 19:37:36 +0100

    Primer commit

---
+++ assets/js/map.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    setTimeout(() =>{
+    document.querySelector("#load-iframe-map").innerHTML = 
+    ` <iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwith="0"
+    loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24334.919309741505!2d-3.8882308025278767!3d40.323155492330386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd418dd5cd83c237%3A0x1c298bd038d71292!2sM%C3%B3stoles%2C%20Madrid!5e0!3m2!1ses!2ses!4v1729756063600!5m2!1ses!2ses">
+    </iframe>`;
+    },800);
+
+});
